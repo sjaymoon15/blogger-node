35be26ebdfab4891cc43cf76c139cbad4216176c
Seungjae Moon <sjaymoon15@gmail.com>
2019-08-20 10:11:15 -0500

    Test clicking login button triggering oauth flow

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,10 +1,27 @@
 // in puppeteer, Page represents one individual tab
 const puppeteer = require('puppeteer');
+let browser, page;
 
-test('We can launch a browser', async () => {
+beforeEach(async () => {
   // browser represents a browser window
-  const browser = await puppeteer.launch({
+  browser = await puppeteer.launch({
     headless: false
   }); // puppeteer almost always async
-  const page = await browser.newPage();
+  page = await browser.newPage();
+  await page.goto('localhost:3000');
+});
+
+afterEach(async () => {
+  await browser.close();
+});
+
+test('The header has the correct text', async () => {
+  const logoText = await page.$eval('a.brand-logo', el => el.innerHTML);
+  expect(logoText).toEqual('Blogster');
+});
+
+test('Clicking login starts oauth flow', async () => {
+  await page.click('.right a');
+  const url = await page.url();
+  expect(url).toMatch(/accounts\.google.com/);
 });

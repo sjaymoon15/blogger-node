f4657116c21f3cb15ceffb84ea4bdcac0f9132fa
Seungjae Moon <sjaymoon15@gmail.com>
2019-08-21 10:32:37 -0500

    Test if header shows logout button after succesful sign in

--- tests/header.test.js
+++ tests/header.test.js
@@ -12,7 +12,7 @@ beforeEach(async () => {
 });
 
 afterEach(async () => {
-  await browser.close();
+  // await browser.close();
 });
 
 test('The header has the correct text', async () => {
@@ -24,4 +24,34 @@ test('Clicking login starts oauth flow', async () => {
   await page.click('.right a');
   const url = await page.url();
   expect(url).toMatch(/accounts\.google.com/);
+});
+
+test.only('When signed in, shows logout button', async () => {
+  const id = '5d5c0dbbaea52a1cd56f8600';
+
+  const Buffer = require('safe-buffer').Buffer;
+  const sessionObject = {
+    passport: {
+      user: id
+    }
+  };
+  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString(
+    'base64'
+  );
+  const Keygrip = require('keygrip');
+  const keys = require('../config/keys');
+  const keygrip = new Keygrip([keys.cookieKey]);
+  const sig = keygrip.sign('session=' + sessionString);
+
+  await page.setCookie({ name: 'session', value: sessionString });
+  await page.setCookie({ name: 'session.sig', value: sig });
+
+  await page.goto('localhost:3000'); // refreshes the page. mimicking sign in.
+
+  await page.waitFor('a[href="/auth/logout"]');
+  const logoutText = await page.$eval(
+    'a[href="/auth/logout"]',
+    el => el.innerHTML
+  );
+  expect(logoutText).toEqual('Logout');
 });

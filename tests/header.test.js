3a7870d518ca9dd3dde07939449002fbbddaf392
Seungjae Moon <sjaymoon15@gmail.com>
2019-08-23 15:17:52 -0500

    Create user, session factories for testing and also create setup file for jest setup.

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,5 +1,7 @@
 // in puppeteer, Page represents one individual tab
 const puppeteer = require('puppeteer');
+const sesstionFactory = require('./factories/sessionFactory');
+const userFactory = require('./factories/userFactory');
 let browser, page;
 
 beforeEach(async () => {
@@ -12,7 +14,7 @@ beforeEach(async () => {
 });
 
 afterEach(async () => {
-  // await browser.close();
+  await browser.close();
 });
 
 test('The header has the correct text', async () => {
@@ -26,29 +28,15 @@ test('Clicking login starts oauth flow', async () => {
   expect(url).toMatch(/accounts\.google.com/);
 });
 
-test.only('When signed in, shows logout button', async () => {
-  const id = '5d5c0dbbaea52a1cd56f8600';
+test('When signed in, shows logout button', async () => {
+  const user = await userFactory(); //userFactory returns a Promise.
+  const { session, sig } = sesstionFactory(user);
 
-  const Buffer = require('safe-buffer').Buffer;
-  const sessionObject = {
-    passport: {
-      user: id
-    }
-  };
-  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString(
-    'base64'
-  );
-  const Keygrip = require('keygrip');
-  const keys = require('../config/keys');
-  const keygrip = new Keygrip([keys.cookieKey]);
-  const sig = keygrip.sign('session=' + sessionString);
-
-  await page.setCookie({ name: 'session', value: sessionString });
+  await page.setCookie({ name: 'session', value: session });
   await page.setCookie({ name: 'session.sig', value: sig });
-
   await page.goto('localhost:3000'); // refreshes the page. mimicking sign in.
-
   await page.waitFor('a[href="/auth/logout"]');
+
   const logoutText = await page.$eval(
     'a[href="/auth/logout"]',
     el => el.innerHTML
---
+++ tests/setup.js
@@ -0,0 +1,7 @@
+// common setup for jest test
+require('../models/User');
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI, { useMongoClient: true });

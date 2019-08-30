f765e93c3a89d0e0ace6f9963840720bbc402c4d
Seungjae Moon <sjaymoon15@gmail.com>
2019-08-30 11:09:05 -0500

    Test the title and content form when valid inputs are typed

--- tests/setup.js
+++ tests/setup.js
@@ -1,5 +1,6 @@
 // jest timeout setup
-jest.setTimeout(30000);
+jest.setTimeout(90000);
+Number.prototype._called = {}; // temp fix for TypeError: Cannot create property '_called' on number '10'...
 
 // common setup for jest test
 require('../models/User');
